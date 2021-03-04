5ea05c911ff21c6e105578f52f7e33186ee500e7
o-xian999 <onoyama1999910@gmail.com>
2021-03-04 15:31:37 +0900

    Make main flamework

---
+++ src/components/Thanks.svelte
@@ -0,0 +1,12 @@
+<style>
+  h1 {
+    font-size: 50px;
+    color: #333333;
+    font-weight: bold;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    margin: 80px 80px;
+    text-align: center;
+  }
+</style>
+
+<h1>Thanks!!!!</h1>
