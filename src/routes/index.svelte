5f0aa73b673ac10bad2f3566d1359d17df313ca9
o-xian999 <onoyama1999910@gmail.com>
2021-03-07 01:52:07 +0900

    Make code component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
+  import Code from "../components/Code.svelte";
   import Thanks from "../components/Thanks.svelte";
 
   let completedKey: Boolean = false;
-  let displayedCode: String = "L-{'32({(8)66-o{'\\\"+6{y'3:-0(83,8,/:'6ym";
 
   const secretKey: Array<String> = [
     't', 'h', 'a', 'n', 'k', 's', /* "thanks_" */ 
@@ -19,22 +19,11 @@
   }
 </script>
 
-<style>
-  h1 {
-    font-size: 50px;
-    color: #333333;
-    font-weight: bold;
-    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
-    margin: 80px 80px;
-    text-align: center;
-  }
-</style>
-
 <svelte:window on:keydown={displaySecretPage}/>
 
 <!-- <button on:click={() => completedKey = true}>Press!!</button> -->
 {#if !completedKey}
-  <h1>{displayedCode}</h1>
+  <Code/>
 {:else}
   <Thanks/>
 {/if}
