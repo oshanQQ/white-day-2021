ab12d09a9f226d314b6efae004795457e1ee6a75
o-xian999 <onoyama1999910@gmail.com>
2021-03-07 23:53:55 +0900

    Embed deploy URL into cipher.c

--- cipher.c
+++ cipher.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-  char cipher[100] = { "URL" };
+  char cipher[100] = { "3''+(all$32'6n7:\"nikijm%6)86/m:++l" };
 
   for (int i = 0; cipher[i] != '\0'; i++) {
     cipher[i] = 'Z' - cipher[i] + 'A';
