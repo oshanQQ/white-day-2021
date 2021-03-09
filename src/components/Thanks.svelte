4b1824afd025f28b309cc63fdfc636976f62b05a
o-xian999 <onoyama1999910@gmail.com>
2021-03-09 16:49:49 +0900

    Write thanks message

--- src/components/Thanks.svelte
+++ src/components/Thanks.svelte
@@ -1,12 +1,33 @@
 <style>
   h1 {
-    font-size: 50px;
     color: #333333;
-    font-weight: bold;
-    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
-    margin: 80px 80px;
     text-align: center;
+    font-size: 4rem;
+    font-weight: 800;
+    letter-spacing: -.06em;
+    margin: 100 100;
+  }
+  .thanks {
+    background-image: linear-gradient(
+      -45deg,
+      #ffcccc 25%,
+      #ffeff7 25%,
+      #ffeff7 50%,
+      #ffcccc  50%,
+      #ffcccc 75%,
+      #ffeff7 75%,
+      #ffeff7
+      );
+    background-size: 40px 40px;
   }
 </style>
 
-<h1>Thanks!!!!</h1>
+<div class="thanks">
+  <h1>Thanks, your chocolate!!!!</h1>
+  <h1>わざわざバレンタイン用意してくれて</h1>
+  <h1>ありがとうございました！！</h1>
+  <h1>C言語の暗号もテンションぶち上がったから</h1>
+  <h1>お返しもこういう形にしてもろたで！！</h1>
+  <h1>もうすぐ後輩も入ってくるし</h1>
+  <h1>来年度もよろしくやで！！！</h1>
+</div>
